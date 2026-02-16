8e774c64afff1ba689f6ccd83378c5dba3aa61ab
Raufur <raufurshekh45@gmail.com>
2026-02-16 07:27:21 +0600

    dashboard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { ExpenceContextProvider } from '../context/ExpenceContext'
+import DashboardLayout from '../layouts/DashboardLayout'
 
 const Home = () => {
   return (
    <ExpenceContextProvider>
-    
+    <DashboardLayout>
+      
+    </DashboardLayout>
    </ExpenceContextProvider>
   )
 }
